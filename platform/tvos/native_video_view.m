9e2cd7ac1a4bf2bc08a655e620cbb00f438f6b37
Valerii <dtho47@yandex.ru>
2022-02-07 12:36:44 +1000

    upd tvos

--- platform/tvos/native_video_view.m
+++ platform/tvos/native_video_view.m
@@ -236,8 +236,8 @@
 - (void)unpauseVideo {
 	[self.avPlayer play];
 	self.isVideoCurrentlyPlaying = YES;
-	[self.avPlayer volume:1.0];
-	NSLog(@"AVPlayer status:%@", [self.avPlayer status]);
+	self.avPlayer.volume = 1.0;
+	NSLog(@"AVPlayer status:%ld", [self.avPlayer status]);
 }
 
 - (void)playerItemDidReachEnd:(NSNotification *)notification {
